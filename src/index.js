2e98119d02919d98d8aea457569470eed3e76dd5
lx kish <urban.ktulhu@gmail.com>
2018-08-08 21:09:36 +1200

    102 Intro and setting up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes'
 
-const App = () => {
-    return(
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import Routes from './routes';
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore)
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
-            <Routes/>
+            <Routes />
         </BrowserRouter>
-    )
-}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    </Provider>
+
+    , document.getElementById('root'));
 
